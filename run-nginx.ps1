529694c173d8d9fa12180008376504903804f57d
TB <q0015300153@gmail.com>
2022-02-15 16:48:52 +0800

    feat: 修改開發用腳本執行動作

--- run-nginx.ps1
+++ run-nginx.ps1
@@ -4,6 +4,8 @@ Set-PsEnv
 
 $path=$(pwd)
 $APP_PATH="${path}\nginx"
-docker build -t my:nginx -f ${APP_PATH}/Dockerfile-dev ${APP_PATH} --no-cache
-docker run -d -i -t --name my_nginx -v ${APP_PATH}:/app -p 8000:80 my:nginx
-docker exec -i -t my_nginx /bin/sh
+#docker build -t my:nginx -f ${APP_PATH}/Dockerfile-dev ${APP_PATH} --no-cache
+#docker run -d -i -t --name my_nginx -v ${APP_PATH}:/app -p 8000:80 my:nginx
+#docker exec -i -t my_nginx /bin/sh
+
+docker exec -i -t app-nginx /bin/bash -c "${args}"
