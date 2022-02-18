11abc85fe1e17af2b384c1e3b4df314e40734bb7
TB <q0015300153@gmail.com>
2022-02-18 18:03:07 +0800

    feat: init vue project

--- first_django/first_vue/src/router/index.js
+++ first_django/first_vue/src/router/index.js
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-const routes: Array<RouteRecordRaw> = [
+const routes = [
   {
     path: '/',
     name: 'home',
@@ -13,6 +13,11 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/hello',
+    name: 'hello',
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
