abd53ac57433b2c77e36e42fc8d4e0879e1b5bc9
TB <q0015300153@gmail.com>
2022-02-18 15:16:17 +0800

    feat: 安裝 python 的 postgresql 套件

--- first_django/first_django/settings.py
+++ first_django/first_django/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'first_django.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'first_django',
+        'USER': 'django',
+        'PASSWORD': 'AemFv26V-+B%#x.Y',
+        'HOST': 'app-postgresql',
+        'PORT': '5432',
     }
 }
 
