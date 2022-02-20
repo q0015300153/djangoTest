2bb4dd63a59737fd620369f0af831ec3ac03a280
TB <q0015300153@gmail.com>
2022-02-20 21:49:20 +0800

    feat: update vue

--- first_django/first_vue/src/router/index.js
+++ first_django/first_vue/src/router/index.js
@@ -14,11 +14,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/hello',
-    name: 'hello',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
--- first_django/first_vue/vue.config.js
+++ first_django/first_vue/vue.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack: {
+    devServer: {
+      watchOptions: {
+        aggregateTimeout: 200,
+        poll: 1000,
+      }
+    }
+  }
 })
